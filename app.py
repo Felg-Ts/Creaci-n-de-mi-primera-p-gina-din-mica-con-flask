9f79163d74b636d2c5be01166084721e19237163
Felg-Ts <el-feli-rubio1996@hotmail.com>
2021-04-20 17:48:22 +0200

    preparando función potencia

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask, render_template
 app = Flask(__name__)	
 
 @app.route('/')
-def inicio():
+def principal():
     return render_template("principal.html")
+
+
+@app.route('/potencia/<int:bp>/<int:ep>')
+def potencia(bp, ep):
+    return render_template("potencia.html", base=bp, exponente=ep, solucion=bp**ep)
+
+@app.route('/cuentaletras/')
+def cuentaletras():
+
+    return render_template("cuentaletras.html")
+
 
 
 app.run("0.0.0.0",5000,debug=True)
